eb90c1fcb80ef0b72e7c7f17be9e24ce0861680e
tvorozochek <89125683472i@gmail.com>
2025-12-18 02:23:23 +0300

    Add files via upload

---
+++ ChatClient.cpp
@@ -0,0 +1,69 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
+#include <windows.h>
+#include <string>
+#include <iostream>
+
+#pragma comment(lib, "ws2_32.lib")
+using namespace std;
+
+SOCKET Connection;
+enum Packet { Pack, PrivatePack, Test };
+
+DWORD WINAPI ClientThread(LPVOID sock) {
+    SOCKET Con = *(SOCKET*)sock;
+    while (true) {
+        Packet packettype;
+        if (recv(Con, (char*)&packettype, sizeof(Packet), NULL) <= 0) break;
+
+        if (packettype == Pack) {
+            int msg_size;
+            recv(Con, (char*)&msg_size, sizeof(int), NULL);
+            char* msg = new char[msg_size + 1];
+            msg[msg_size] = 0;
+            recv(Con, msg, msg_size, NULL);
+            cout << msg << endl;
+            delete[] msg;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    WSAData wsaData;
+    WSAStartup(MAKEWORD(2, 1), &wsaData);
+
+    SOCKADDR_IN addr;
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_port = htons(123);
+    addr.sin_family = AF_INET;
+
+    Connection = socket(AF_INET, SOCK_STREAM, NULL);
+    if (connect(Connection, (SOCKADDR*)&addr, sizeof(addr)) != 0) {
+        cout << "Error: failed connect to server.\n";
+        return 1;
+    }
+
+    cout << "Connected! Enter your name: ";
+    string name;
+    getline(cin, name);
+
+    // Отправляем имя серверу первым делом
+    int name_size = name.size();
+    send(Connection, (char*)&name_size, sizeof(int), NULL);
+    send(Connection, name.c_str(), name_size, NULL);
+
+    CreateThread(NULL, NULL, ClientThread, &Connection, NULL, NULL);
+
+    cout << "To send private msg use: @Name message" << endl;
+    string msg;
+    while (true) {
+        getline(cin, msg);
+        int msg_size = msg.size();
+        Packet type = Pack;
+        send(Connection, (char*)&type, sizeof(Packet), NULL);
+        send(Connection, (char*)&msg_size, sizeof(int), NULL);
+        send(Connection, msg.c_str(), msg_size, NULL);
+    }
+    return 0;
+}
---
+++ ChatServer.cpp
@@ -0,0 +1,113 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
+#include <windows.h>
+#include <string>
+#include <iostream>
+
+#pragma comment(lib, "ws2_32.lib")
+using namespace std;
+
+SOCKET Connections[100];
+string Names[100]; // Массив для имен
+int Counter = 0;
+
+enum Packet { Pack, PrivatePack, Test };
+
+DWORD WINAPI ServerThread(LPVOID param) {
+    int index = *(int*)param;
+    delete (int*)param; // Очищаем память, выделенную под индекс
+    SOCKET Con = Connections[index];
+
+    while (true) {
+        Packet packettype;
+        if (recv(Con, (char*)&packettype, sizeof(Packet), NULL) <= 0) break;
+
+        if (packettype == Pack) {
+            int msg_size;
+            recv(Con, (char*)&msg_size, sizeof(int), NULL);
+            char* msg = new char[msg_size + 1];
+            msg[msg_size] = 0;
+            recv(Con, msg, msg_size, NULL);
+
+            string messageStr = msg;
+            cout << "[" << Names[index] << "]: " << messageStr << endl; // Сервер видит всё
+
+            // Проверка на личное сообщение: @Имя Текст
+            if (messageStr[0] == '@') {
+                size_t spacePos = messageStr.find(' ');
+                if (spacePos != string::npos) {
+                    string targetName = messageStr.substr(1, spacePos - 1);
+                    string privateMsg = "(Private) " + Names[index] + ": " + messageStr.substr(spacePos + 1);
+
+                    int p_size = privateMsg.size();
+                    bool found = false;
+                    for (int i = 0; i < Counter; i++) {
+                        if (Names[i] == targetName) {
+                            Packet pType = Pack;
+                            send(Connections[i], (char*)&pType, sizeof(Packet), NULL);
+                            send(Connections[i], (char*)&p_size, sizeof(int), NULL);
+                            send(Connections[i], privateMsg.c_str(), p_size, NULL);
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            else {
+                // Обычный Broadcast (всем)
+                string broadcastMsg = Names[index] + ": " + messageStr;
+                int b_size = broadcastMsg.size();
+                for (int i = 0; i < Counter; i++) {
+                    if (i == index) continue;
+                    Packet pType = Pack;
+                    send(Connections[i], (char*)&pType, sizeof(Packet), NULL);
+                    send(Connections[i], (char*)&b_size, sizeof(int), NULL);
+                    send(Connections[i], broadcastMsg.c_str(), b_size, NULL);
+                }
+            }
+            delete[] msg;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    WSAData wsaData;
+    WSAStartup(MAKEWORD(2, 1), &wsaData);
+
+    SOCKADDR_IN addr;
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_port = htons(123);
+    addr.sin_family = AF_INET;
+
+    SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);
+    bind(sListen, (SOCKADDR*)&addr, sizeof(addr));
+    listen(sListen, 10);
+
+    cout << "Server started..." << endl;
+
+    for (int i = 0; i < 100; i++) {
+        int sizeofaddr = sizeof(addr);
+        SOCKET newCon = accept(sListen, (SOCKADDR*)&addr, &sizeofaddr);
+
+        if (newCon != INVALID_SOCKET) {
+            // Сначала получаем имя клиента
+            int name_size;
+            recv(newCon, (char*)&name_size, sizeof(int), NULL);
+            char* name = new char[name_size + 1];
+            name[name_size] = 0;
+            recv(newCon, name, name_size, NULL);
+
+            Connections[Counter] = newCon;
+            Names[Counter] = name;
+
+            cout << "Client Connected: " << Names[Counter] << endl;
+
+            int* index = new int(Counter);
+            CreateThread(NULL, NULL, ServerThread, index, NULL, NULL);
+            Counter++;
+            delete[] name;
+        }
+    }
+    return 0;
+}
